7c3f4329caf2e8c536de2f1d61a81b64fb5509f2
Thiago Maylon <tmaylon88@gmail.com>
2023-12-15 17:04:46 -0300

    aula 4

---
+++ aula04.py
@@ -0,0 +1,43 @@
+'''
+    overrride de métodos
+    polimofismo
+    destrutores
+'''
+
+class ContaBancaria(object):
+    def __init__(self):
+        self.saldo = 0
+
+    def depositar(self, valor):
+        self.saldo += valor
+        self.ver_saldo()
+    
+    def sacar(self, valor):
+        self.saldo -= valor
+        self.ver_saldo()
+    
+    def ver_saldo(self):
+        print(f"seu Saldo é de {self.saldo:.2f}")
+
+class ContaPoupanca(ContaBancaria):
+    rentabilidade_total = 1.6
+    def _consulta_rentabilidade(self): # método privado
+        return self.rentabilidade_total
+    
+    def rentabilidade(self):
+        rentabilidade = self._consulta_rentabilidade()
+
+        if(rentabilidade < 0.5):
+            print('consulte o seu gerente')
+        else:
+            print(rentabilidade)
+    
+    def depositar(self, valor): # mudando o comportamento do método herdado da classe Contabancaria
+        self.saldo += valor
+        if self.saldo >= 1000:
+            self.rentabilidade_total += 0.1
+            print(f'Parabens, sua retabilidade aumentou para {self.rentabilidade_total:.2f}')
+        self.ver_saldo()
+    
+conta_poupanca = ContaPoupanca()
+conta_poupanca.depositar(1000)
